4d9bf0548b08a82e63514cb047f38a73b957067d
Ivan Kolyshnitsyn <ewan.kolyshnitsyn@yandex.ru>
2025-04-05 14:25:59 +0300

    Версия 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,28 @@
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// @todo: Функция создания карточки 1
 
-// @todo: Функция удаления карточки
+// @todo: Функция удаления карточки 
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+
+const cardTemplate = document.querySelector('#card-template').content;
+
+function card(link, name) {
+    const lacesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    
+    lacesItem.querySelector('.card__image').src = link;
+    lacesItem.querySelector('.card__title').textContent = name;
+
+    placesList.append(lacesItem);
+    
+    lacesItem.querySelector('.card__delete-button').addEventListener('click', function () {
+        lacesItem.remove();
+    });
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+    card(initialCards[i].link, initialCards[i].name);
+}
