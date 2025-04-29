33d1ae922e67980814389feb221978416693ce46
Ivan Kolyshnitsyn <ewan.kolyshnitsyn@yandex.ru>
2025-04-29 14:49:43 +0300

    Почти готово

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,21 @@ const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
 
-function createCard(data) {
+function createCard(data, openPopupImage, like) {
     const lacesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
     
-    lacesItem.querySelector('.card__image').src = data.link;
-    lacesItem.querySelector('.card__title').textContent = data.name;
-    lacesItem.querySelector('.card__image').alt = `Пейзаж местности ${lacesItem.querySelector('.card__title').textContent}`;
+    const name = data.name;
+    const link = data.link;
+    const alt = lacesItem.querySelector('.card__title').textContent;
+    const likeButton = lacesItem.querySelector('.card__like-button');
+
+    lacesItem.querySelector('.card__image').src = link;
+    lacesItem.querySelector('.card__title').textContent = name;
+    lacesItem.querySelector('.card__image').alt = `Пейзаж местности ${alt}`;
 
     lacesItem.querySelector('.card__delete-button').addEventListener('click', () => handleDeleteCard(lacesItem));
+    lacesItem.querySelector('.card__image').addEventListener('click', () => openPopupImage(name, link));
+    lacesItem.querySelector('.card__like-button').addEventListener('click', () => like(likeButton))
 
     return lacesItem;
 }
@@ -24,7 +31,7 @@ function renderCard(card) {
 }
 
 for (let i = 0; i < initialCards.length; i++) {
-    renderCard(createCard(initialCards[i]));
+    renderCard(createCard(initialCards[i], openPopupImage, like));
 }
 
 
@@ -47,7 +54,6 @@ function openPopup(popup) {
 
 function closePopup(popup) {
     popup.classList.remove('popup_is-opened');
-
 }
 
 profileEditButton.addEventListener('click', () => openPopup(popopEdit));
@@ -58,14 +64,10 @@ closePopupNewCard.addEventListener('click', () => closePopup(popupNewCard));
 
 
 
-
-
-
-
-
 const formEdit = document.forms['edit-profile'];
 const formEditName = formEdit.elements.name;
 const formEditDescription = formEdit.elements.description;
+
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -85,49 +87,49 @@ formEdit.addEventListener('submit', handleFormSubmit)
 
 
 
-
-
-
 const formNewPlace = document.forms['new-place'];
 const formCardName = formNewPlace.elements['place-name'];
 const formCardLink = formNewPlace.elements.link;
 
+function renderNewCard(card) {
+    placesList.prepend(card);
+}
+
 function handleNewCard(evt) {
+    
     evt.preventDefault();
+
     const newCard = {
         name: formCardName.value,
         link: formCardLink.value
     }
+
+    renderNewCard(createCard(newCard, openPopupImage, like));
     
-    initialCards.unshift(newCard);
-
-    renderCard(createCard(initialCards[0]));
-
     formCardName.value = '';
     formCardLink.value = '';
 
     closePopup(popupNewCard)
 }
+
 formNewPlace.addEventListener('submit', handleNewCard)
 
 
+function openPopupImage(name, link) {
+    const popupTypeImage = document.querySelector('.popup_type_image');
+    const closePopupImage = popupTypeImage.querySelector('.popup__close');
 
-const popupTypeImage = document.querySelector('.popup_type_image');
-const closePopupImage = popupTypeImage.querySelector('.popup__close');
-
-const card = placesList.querySelector('.places__item ')
-const cardText = card.querySelector('.card__title');
-const cardImage = card.querySelector('.card__image');
-
-function openPopupImage() {
     const popupImage = popupTypeImage.querySelector('.popup__image');
     const popupText = popupTypeImage.querySelector('.popup__caption');
     
-    popupImage.src = cardImage.src;
-    popupImage.alt = cardImage.alt;
-    popupText.textContent = cardText.textContent;
-    openPopup(popupTypeImage)
+    popupImage.src = link;
+    popupText.textContent = name;
+    popupImage.alt = name;
+    openPopup(popupTypeImage);
+
+    closePopupImage.addEventListener('click', () => closePopup(popupTypeImage))
 }
 
-closePopupImage.addEventListener('click', () => closePopup(popupTypeImage))
-cardImage.addEventListener('click', () => openPopupImage())
+function like(item) {
+    item.classList.toggle('card__like-button_is-active');
+}
