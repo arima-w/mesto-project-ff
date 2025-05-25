90567c214ea278f9d4ce4004ef6f6b3e8525f3dc
Ivan Kolyshnitsyn <ewan.kolyshnitsyn@yandex.ru>
2025-05-25 13:55:26 +0300

    1

--- src/components/validation.js
+++ src/components/validation.js
@@ -30,6 +30,7 @@ const checkInputValidity = (formElement, inputElement) => {
 }
 
 
+
 const setEventListeners = (formElement) => {
     const inputList = Array.from(formElement.querySelectorAll('.popup__input'))
     const buttonElement = formElement.querySelector('.popup__button');
