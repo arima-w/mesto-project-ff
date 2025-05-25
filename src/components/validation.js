ed5d100a4085f6a4c27d4250c8e5e8f3d01b01a4
Ivan Kolyshnitsyn <ewan.kolyshnitsyn@yandex.ru>
2025-05-25 13:30:09 +0300

    проверка

--- src/components/validation.js
+++ src/components/validation.js
@@ -29,6 +29,7 @@ const checkInputValidity = (formElement, inputElement) => {
     }
 }
 
+
 const setEventListeners = (formElement) => {
     const inputList = Array.from(formElement.querySelectorAll('.popup__input'))
     const buttonElement = formElement.querySelector('.popup__button');
